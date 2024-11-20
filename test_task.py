4a8067384b914973dd7facb6ad6647e4159447a7
Ryukamis <118356127+Ryukamis@users.noreply.github.com>
2024-11-20 15:32:44 +0100

    Update test_task.py

--- test_task.py
+++ test_task.py
@@ -1,14 +1,16 @@
 import unittest
 import numpy as np
 import numpy.testing as npt
+import task
 
 class TestMatrixMultiplication(unittest.TestCase):
 
     def test_matrix_multiplication(self):
         # Exemple de matrices A, x et B
-        A = np.array([[1, 2], [3, 4]])  # Matrice A (2x2)
-        x = np.array([10, 30])            # Vecteur x (2x1)
-        B = np.array([17, 39])          # Résultat attendu B (2x1)
+        T=task()
+        A = T.a
+        x = T.x           # Vecteur x (2x1)
+        B = T.b       # Résultat attendu B (2x1)
 
         result = np.dot(A, x)
 
