80ed79524c7ccd94e72d151a612732e9efdde01d
Ryukamis <118356127+Ryukamis@users.noreply.github.com>
2024-12-16 13:58:26 +0100

    Update QueueManager.py

--- QueueManager.py
+++ QueueManager.py
@@ -5,14 +5,21 @@ from queue import Queue
 task_queue = Queue()
 result_queue = Queue()
 
-class QueueManager(BaseManager): pass
+class QueueManager(BaseManager):
+    def __init__(self, url: str, port: int, authkey: bytes):
+        super().__init__(address=(url, port), authkey=authkey)
 
+        self.task_queue = mp.Queue()
+        self.result_queue = mp.Queue()
+
+        self.register("get_task_queue", callable=lambda: self.task_queue)
+        self.register("get_result_queue", callable=lambda: self.result_queue)
+        
+    def serve(self):
+        print(f"Starting server at {self.address[0]}:{self.address[1]} ...")
+        s = self.get_server()
+        s.serve_forever()
     
-if __name__ == '__main__':    
-    QueueManager.register('task_queue')
-    QueueManager.register('result_queue')
-    m = QueueManager(address=('', 50000), authkey=b'abracadabra')
-
-    s = m.get_server()
-
-    s.serve_forever()
+if __name__ == "__main__":
+    manager = QueueManager("localhost", 50000, b"abc123")
+    manager.serve()
