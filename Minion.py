c66d81f85ddf9687637bfb9375a4788565420b5b
Ryukamis <118356127+Ryukamis@users.noreply.github.com>
2024-12-18 13:47:16 +0100

    Update Minion.py

--- Minion.py
+++ Minion.py
@@ -1,24 +1,11 @@
-from queue_client import QueueClient
+import queue_mc as q
 from task import Task
 
+qc = q.QueueClient()
 
-class Minion(QueueClient):
-    def __init__(self):
-        super().__init__()
-
-    def run(self):
-        while True:
-            Task = self.get_task(self.task_queue)
-            if Task is None:
-                continue
-
-            print(f"reçu #{task.identifier}")
-            task.work()
-            print(f"terminé #{task.identifier} en {task.time} secondes")
-
-            self.result_queue.put(task)
-
-
-if __name__ == "__main__":
-    minion = Minion()
-    minion.run()
+result = Task()
+while True:
+    t = qc.taskQueue.get()
+    t.work()
+    print("tache : " + str(t.identifier) + " en : " + str(t.time))
+    qc.resultQueue.put(t)
