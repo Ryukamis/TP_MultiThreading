360310d96c6866b98c1549fb35e59d6a2d7f7371
Ryukamis <118356127+Ryukamis@users.noreply.github.com>
2024-12-18 13:46:13 +0100

    Update Boss.py

--- Boss.py
+++ Boss.py
@@ -1,45 +1,27 @@
-import threading
+import queue_mc as q
+from task import Task
 import time
 
-import numpy as np
-from queue_client import QueueClient
-from task import Task
+# Initialisation du client de file d'attente
+qc = q.QueueClient()
 
+# Générateur de tâches
+task_id = 1  # Compteur pour générer des identifiants de tâches
+task_result = Task()
+while True:
+    # Création et ajout d'une nouvelle tâche toutes les secondes
+    new_task = Task(task_id, 1000)  # Exemple de tâche avec ID et paramètre
+    qc.taskQueue.put(new_task)
+    print(f"Tâche ajoutée à la file d'attente :  {new_task.identifier}")
+    task_id += 1
 
-class Boss(QueueClient):
-    def __init__(self, task_size: int = 10):
-        super().__init__()
-        self.task_size = task_size
+    # Récupération des résultats
+    try:
+        while not qc.resultQueue.empty():
+            task_result = qc.resultQueue.get()
+            print(f"Tache récupéré : {task_result.identifier} en  {task_result.time} s")
+    except Exception as e:
+        print(f"Erreur lors de la récupération des résultats : {e}")
 
-    def read_result_queue(self):
-        while True:
-            task = self.get_task(self.result_queue)
-            if task is None:
-                continue
-
-            res = np.linalg.norm(task.a @ task.x - task.b)
-            print(
-                f"Result for #{task.identifier}: {'valid' if res < 1e-4 else f'invalid {res}'}"
-            )
-
-    def run(self):
-        i = 1
-
-        read_thread = threading.Thread(target=self.read_result_queue, daemon=True)
-        read_thread.start()
-
-        while True:
-            task = Task(f"Task {i}", self.task_size)
-
-            self.task_queue.put(task)
-            i += 1
-            time.sleep(0.5)
-
-
-if __name__ == "__main__":
-    import sys
-
-    task_size = int(sys.argv[1]) if len(sys.argv) > 1 else 10
-
-    boss = Boss(task_size=task_size)
-    boss.run()
+    # Pause d'une seconde avant de poster une nouvelle tâche
+    time.sleep(1)
