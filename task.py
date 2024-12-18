e7536366cc3db42ac86e84a84975e0cf6134cfab
Ryukamis <118356127+Ryukamis@users.noreply.github.com>
2024-12-18 13:48:48 +0100

    Update task.py

--- task.py
+++ task.py
@@ -1,64 +1,52 @@
+import time
 import json
-from time import perf_counter
-
 import numpy as np
 
 
 class Task:
-    def __init__(self, identifier: str, size: int = 10):
-        """Initializes a task with a random matrix A and vector B"""
-
+    def __init__(self, identifier=0, size=None):
         self.identifier = identifier
-        self.size = size
-
-        self.a = np.random.rand(size, size)
-        self.b = np.random.rand(size)
-        self.x = np.zeros(size)
-
+        # choosee the size of the problem
+        self.size = size or np.random.randint(300, 3_000)
+        # Generate the input of the problem
+        self.a = np.random.rand(self.size, self.size)
+        self.b = np.random.rand(self.size)
+        # prepare room for the results
+        self.x = np.zeros((self.size))
         self.time = 0
 
     def work(self):
-        """Time intensive function that finds X where A = X * B"""
-
-        start = perf_counter()
+        start = time.perf_counter()
         self.x = np.linalg.solve(self.a, self.b)
-        self.time = perf_counter() - start
+        self.time = time.perf_counter() - start
 
     def to_json(self) -> str:
         return json.dumps(
             {
-                "identifier": self.identifier,
-                "size": self.size,
                 "a": self.a.tolist(),
                 "b": self.b.tolist(),
                 "x": self.x.tolist(),
                 "time": self.time,
-            }
+                "id": self.identifier,
+            },
+            sort_keys=True,
+            indent=4,
         )
 
-    def from_json(cls, json_str: str) -> "Task":
-        obj = json.loads(json_str)
-        task = Task("x")
+    @staticmethod
+    def from_json(text: str) -> "Task":
+        j = json.loads(text)
+        t = Task()
+        t.a = np.array(j["a"])
+        t.b = np.array(j["b"])
+        t.x = np.array(j["x"])
+        t.time = np.array(j["time"])
+        t.identifier = np.array(j["id"])
 
-        task.identifier = obj["identifier"]
-        task.size = obj["size"]
-        task.a = np.array(obj["a"])
-        task.b = np.array(obj["b"])
-        task.x = np.array(obj["x"])
-        task.time = obj["time"]
+        return t
 
-        return task
+    def __eq__(self, other: "Task") -> bool:
+        return np.array_equal(self.a, other.a) and np.array_equal(self.b, other.b)
 
-    def __eq__(self, __value: object) -> bool:
-        if not isinstance(__value, Task):
-            return False
-
-        task: Task = __value
-        return (
-            self.identifier == task.identifier
-            and self.size == task.size
-            and np.array_equal(self.a, task.a)
-            and np.array_equal(self.b, task.b)
-            and np.array_equal(self.x, task.x)
-            and self.time == task.time
-        )
+    def __repr__(self):
+        return str(self.a[0, 0])
